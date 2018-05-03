7e3a4d24f809f05d096e9dc875df3520eaa464bf
Keanu Nichols <nichols.keanu9@gmail.com>
2018-05-03 09:16:56 -0400

    Update sel11.py

--- sel11.py
+++ sel11.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import urllib.request
 from bs4 import BeautifulSoup
+from datetime import datetime
+import os
 
 def exchange(): # This part of the program uses the selenium API to open the Republic Bank webpage with the javascript enabled to download the current exchange from from TTD to USD
     driver = webdriver.PhantomJS('./\phantomjs.exe') # Opens the particular driver for my instance I am using Google Chrome and you have to download a specific driver for it for selenium
@@ -40,6 +42,30 @@ def coins():
         place += 1
     return (sum)
 
+def coins1():
+#This part of the program takes a text file that contains the differing cryptocurrencies
+#amount the user has and gets the value and then multiplies it by the current amount the cryptocurrency is at which is provided by the prices function() and prints the amount you have in USD
+
+    fact = False
+    place = 0
+    sum = 0
+    temp = ""
+    file = open("CryptocurrenciesMom.txt", "r")
+    for line in file:
+        for x in line:
+            if(fact == True):
+                temp += x
+            if (x == " "):
+                fact = True
+        num = float(temp.strip())
+        value = num * prices1(place)
+        print("You have %.2f USD\n" %(value))
+        sum += value
+        temp = ""
+        fact = False
+        place += 1
+    return (sum)
+
 def prices(place):
 #This part of the program uses the BeautifulSoup library and takes the html of coinmarket and finds the a tag associated with each cryptocurrency and takes the text associated
 #with that tag and passes that value back to the coins() function
@@ -57,6 +83,25 @@ def prices(place):
     print("%s: 1 %s = %.2f USD" %(currency[place],Symbol[place], a_new))
     return (a_new)
 
+def prices1(place):
+#This part of the program uses the BeautifulSoup library and takes the html of coinmarket and finds the a tag associated with each cryptocurrency and takes the text associated
+#with that tag and passes that value back to the coins() function
+    
+    url = urllib.request.urlopen("https://coinmarketcap.com/all/views/all/")#Had to change from normal coinmarket to include Bitcoin Gold original was https://coinmarketcap.com/
+    soup = BeautifulSoup(url, "html.parser")
+    values = ["a[href*=/currencies/bitcoin/#markets]","a[href*=/currencies/neo/#markets]",
+              "a[href*=/currencies/gas/#markets]", "a[href*=/currencies/substratum/#markets]",
+              "a[href*=/currencies/red-pulse/#markets]","a[href*=/currencies/unikoin-gold/#markets]",
+              "a[href*=/currencies/bounty0x/#markets]"]
+    currency= ["Bitcoin", "Neo", "Gas",  "Substratum", "Red Pulse", "Unikoin Gold", "Bounty0x"]
+    Symbol = ["BTC", "NEO", "GAS", "SUB", "RPX", "UKG", "BNTY"]
+    a = soup.select_one(values[place]).text
+    temp = a.replace("$", "")
+    a_new = float(temp)
+    print("%s: 1 %s = %.2f USD" %(currency[place],Symbol[place], a_new))
+    return (a_new)
+
+
 #The main program starts here
 #The main part of the program calculates how much profit you gained from your initial investment
 #Ans = input("Do you need to convert to USD (Y/N)\n")
@@ -69,15 +114,27 @@ if(Ans == "Y"):
     print("$%d TTD = $%.4f USD" %(money, value))
 initial = 2600
 initial1 = 1000
-print("Your Cryptocurrency:")
+print("Keanu's Cryptocurrency:")
 sum = coins()
 ans = str(round(sum, 2))
 file = open("sum1.txt", "a")
+if(os.stat("sum1.txt").st_size == 0):
+    file.write("Amount\t\t\tPercent Increase\t\tDate\n")
 file.write("$" + ans + " USD\t\t")
+increase = ((sum - initial) / initial) * 100
+inc = str(round(increase, 2))
+file.write(str(inc) + "%\t\t\t\t")
 file.write(str(datetime.now()))
 file.write("\n")
 file.close()
-increase = ((sum - initial) / initial) * 100
 y = "%"
 print("Your running total is %.2f USD and your increase from the initital amount of $%d is %.2f%s" %(sum, initial, increase, y))
 print()
+
+print("Mom's Cryptocurrency:")
+sum1 = coins1()
+increase = ((sum1 - initial1) / initial1) * 100
+y = "%"
+print("Your running total is %.2f USD and your increase from the initital amount of $%d is %.2f%s" %(sum1, initial1, increase, y))
+test = input()
+
